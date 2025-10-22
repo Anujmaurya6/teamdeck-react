bcb2afe9061cd6b1a1cce7bba646463b111099f5
anujmaurya6 <maurya913639@gmail.com>
2025-10-22 23:47:01 +0530

    Initial commit - setup Framer Motion scroll animation cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,21 @@
+import { motion } from "framer-motion";                                    
 import React from 'react'
-import MemberCard from './components/MemberCard'
+
 const App = () => {
-
-  const team=[
-    {
-      name:"Anuj maurya",
-      Age:14,
-      role:"Full Stack developer",
-      about:"Tradtional thinker,forward dreamer", 
-    },
-    {
-      name: "Priya Sharma",
-      role: "Frontend Developer",
-      img: "https://via.placeholder.com/150/FFB6C1/000000",
-      about: "Loves crafting clean and minimal UI ✨"
-    },
-    {
-      name: "Ravi Kumar",
-      role: "AI/ML Enthusiast",
-      img: "https://via.placeholder.com/150/87CEFA/000000",
-      about: "Finds patterns where others see chaos 🤖"
-    },
-    {
-      name: "Priya Sharma",
-      role: "Frontend Developer",
-      img: "https://via.placeholder.com/150/FFB6C1/000000",
-      about: "Loves crafting clean and minimal UI ✨"
-    },
-    {
-      name: "Ravi Kumar",
-      role: "AI/ML Enthusiast",
-      img: "https://via.placeholder.com/150/87CEFA/000000",
-      about: "Finds patterns where others see chaos 🤖"
-    }
-  ]
-
-  const handleClick=(name)=>{
-alert("You have clicked in the ${name} card");
-  }
-
-
   return (
-   <div>
-    <div className='min-h-screen bg-grey-500 flex flex-col items-center py-10'>  
-      <h1 className='text-3xl font-bold text-gray-800 mb-7'>Our Teams</h1>
-      <div className='flex flex-wrap justify-center gap-6'>
-    {team.map((member,index)=>(
-     <MemberCard
-       key={index}
-            name={member.name}
-            role={member.role}
-            img={member.img}
-            about={member.about}
-            onCardClick={handleClick}
-      />
-    ))}
+<div>
+   <motion.div  
+   whileHover={{background:"green"}}
+   whileTap={{scale:0.7}}
+   drag
+   whileDrag={{scale:0.8}}
+   className="bg-red-500 h-40 w-1/2 mt-10 place-self-center  ">
+    subscribe
+   </motion.div>
 
-      </div>
-    </div>
    </div>
-  );
+
+  )
 }
 
 export default App
